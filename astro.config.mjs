915fd34f99445da2035e4a22f71f80f8fb5aa0be
Rebel Ape Marketing <adam@therebelape.com>
2025-11-04 15:16:08 -0700

    More 301 work.

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://therebelape.com',
-  trailingSlash: "always",
+  trailingSlash: "always",  // COMMIT TO THIS
   integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()]
