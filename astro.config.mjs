613a55ccc54f0969f060c313d73efb6b4d077ac3
Rebel Ape Marketing <adam@therebelape.com>
2025-11-04 14:15:22 -0700

    Still reworking the redirects. Changed to yes trailing slash.

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://therebelape.com',
-  trailingSlash: "never",
+  trailingSlash: "always",
   integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()]
